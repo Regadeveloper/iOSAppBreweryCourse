2b78a879d9b66875e669b35cc571aba79dafd0e1
Raúl <raulregadera@gmail.com>
2021-10-31 16:27:59 +0100

    solved with enum and switch

--- 4/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ 4/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
+    enum eggType: Int {
+        case soft = 5
+        case medium = 7
+        case hard = 12
+    }
+    
+    @IBAction func hardnessSelected (sender: UIButton) {
+        let hardness = sender.currentTitle ?? "no tiene nombre"
+
+        switch hardness {
+        case "Soft":
+            print(eggType.soft.rawValue)
+            break
+        case "Medium":
+            print(eggType.medium.rawValue)
+            break
+        case "Hard":
+            print(eggType.hard.rawValue)
+            break
+        default:
+            print("Invalid option")
+            break
+        }
+
+    }
 
 }
