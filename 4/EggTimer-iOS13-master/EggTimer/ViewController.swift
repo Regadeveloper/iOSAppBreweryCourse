ac78f55a6461f245bc4b87a4f6f09447f6c48ba5
Raúl <raulregadera@gmail.com>
2021-11-06 20:54:46 +0100

    added countdown

--- 4/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ 4/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -16,12 +16,40 @@ class ViewController: UIViewController {
         case hard = 12
     } */
 
-    let eggTimes = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
+    let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
+    var timeMissing: Int? = nil
+    var timer: Timer?
+
     
     @IBAction func hardnessSelected (sender: UIButton) {
         guard let hardness = sender.currentTitle else {return}
         guard let timeNeeded = eggTimes[hardness] else {return}
-        print(timeNeeded)
+        if timeMissing == nil {
+            timeMissing = timeNeeded
+            createNewTimer()
+        }
+        else if timeMissing! <= 0 {
+            timeMissing = timeNeeded
+            createNewTimer()
+        }
+    }
+
+        func createNewTimer() {
+            self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
+                if self.timeMissing! > 0 {
+                        print ("\(self.timeMissing!) seconds")
+                        self.timeMissing! -= 1
+                    } else {
+                        if let timer = self.timer {
+                            timer.invalidate()
+                            self.timer = nil
+                        }
+                    }
+            }
+
+
+
+        //print(timeNeeded)
 
         /* switch hardness {
         case "Soft":
