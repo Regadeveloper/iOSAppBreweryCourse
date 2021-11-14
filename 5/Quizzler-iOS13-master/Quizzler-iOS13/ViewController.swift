ae1cefc60cce0a31aaf139e7d2c25cec4a2c151b
Raúl <raulregadera@gmail.com>
2021-11-14 17:27:26 +0100

    used bidimensional array to handle quiz answers

--- 5/Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
+++ 5/Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+
+    let quiz = [
+        ["You're my creator?", "False"],
+        ["You're the player?", "True"],
+        ["You're human?", "True"],
+        ["This is the last question?", "True"]
+    ]
+
+    var questionNumber = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.titleLabel?.text
+        let correctAnswer = quiz[questionNumber] [1]
+        if userAnswer == correctAnswer {
+            print ("correct answer")
+        } else {
+            print ("wrong")
+        }
+        if questionNumber < quiz.count - 1  {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
+
+    func updateUI () {
+        questionLabel.text = quiz[questionNumber][0]
     }
 
 
